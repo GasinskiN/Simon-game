99cb44d998e97487173425dd748a71ece7148359
GasinskiN <gasinskinorman@gmail.com>
2023-02-26 19:30:57 +0100

    Added game over functionality

--- game.js
+++ game.js
@@ -32,6 +32,11 @@ function nextSequence() {
         , 150);
 }
 
+function toggleGameOver() {
+    document.querySelector("body").classList.toggle("game-over");
+    document.querySelector(".game-end-title").classList.toggle("disappear");
+}
+
 // function checks if the start game button is hidden if not returns nothing, than checks if 
 // the button clicked was the right button and plays a sound at the end it checks if the button 
 // we clicked was last in sequence if so it runs nextSequence and resets the numberInSequence 
@@ -44,6 +49,14 @@ function sequnceChecker() {
     if(sequenceArray[numberInSequence] === buttonColor){
         var sound = new Audio("sounds/" + buttonColor + ".mp3");
         sound.play();
+    }else {
+        var sound = new Audio("sounds/wrong.mp3");
+        sound.play();
+        toggleGameOver();
+        $(".start-game").show("fast");
+        sequenceLength = 0;
+        sequenceArray = [];
+        return;
     }
     numberInSequence++;
     if(numberInSequence > sequenceLength){
@@ -59,9 +72,13 @@ function squareHighlight() {
     setTimeout(()=>{this.classList.toggle("pressed")}, 80);
 }
 // Hides start game button and calls the next sequence 
-// after waiting for the button to hidefunction
+// after waiting for the button to hide and toggles the game 
+// over screen if it detects it
 $(".start-game").on("click", function(){
     $(this).hide("fast");
+    if (document.querySelector("body").classList.length > 0){
+        toggleGameOver();
+    }
     setTimeout(nextSequence, 190);
 });
 
